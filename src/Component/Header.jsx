326e803d6f7a3bdbb6313fe9023db69a0ad24e15
SanaIshaqat <sanaishaqat@gmail.com>
2022-01-02 22:25:02 +0200

    mainStyling

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { Nav, Navbar, Container } from 'react-bootstrap';
+import "./Header.css"
+import logo from "../assets/images/SPOTSLOGO00.png"
 function Header() {
   return (
     <div>
-      <div>logo</div>
-      <div>Spots</div>
-      <Link to="/">Home</Link>
-      <Link to="/sign-in">Sign IN</Link>
-      <Link to="/about-us">About Us</Link>
-      <Link to="/servers">Servers list</Link>
-      <Link to="/chat">Chat</Link>
+      <Navbar className="Nav" >
+  <Container>
+  <img
+        src= {logo}
+        width="200"
+        height="200"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/sign-in">Sign-In</Nav.Link>
+      <Nav.Link to="/about-us">About Us</Nav.Link>
+      <Nav.Link to="/servers">Servers list</Nav.Link>
+      <Nav.Link to="/chat">Chat</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
     </div>
   );
 }
 
 export default Header;
+
--- src/Component/Home/HomePage.jsx
+++ src/Component/Home/HomePage.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import './HomePage.css';
+import { Nav, Navbar, Container, Button } from 'react-bootstrap';
+import landing from "../../assets/images/landinsize.jpg";
+import logo from "../../assets/images/SPOTSLOGO00.png"
+
+
 export const HomePage = (props) => {
   const user = useSelector((state) => state.user);
-  return <div>Home</div>;
+  return <>
+    <div className='landing'>
+    <Navbar className="NavBar" >
+  <Container>
+  
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/sign-in">Sign-In</Nav.Link>
+      <Nav.Link to="/about-us">About Us</Nav.Link>
+      <Nav.Link to="/servers">Servers list</Nav.Link>
+      <Nav.Link to="/chat">Chat</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+<div className="landing-header">
+<img
+        src= {logo}
+        width="450"
+        height="100"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+<h1>CONNECT AND SHARE.</h1>
+<h2>FIND YOUR SPOT IN THE WORLD!</h2>
+<Button/>
+</div>
+
+
+
+    </div>
+    
+    </>;
 };
 
 export default HomePage;
