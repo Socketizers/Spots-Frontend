0988fa4fe641c02568332043b8b3c48bce5896a6
BianQt <bayanqutshan@gmail.com>
2022-01-05 14:09:28 +0200

    add footer

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,7 +1,58 @@
-import React from "react";
 
 function Footer() {
-  return <div>My Footer</div>;
+  return <div>
+    <div className="container" >
+	<div className="row" style={{textAlign:'center'}}>
+	</div>
+
+    <footer className="footer-bs">
+        <div className="row">
+        	<div className="col-md-4 footer-brand animated fadeInLeft">
+          <img
+        src= {logo}
+        width="195"
+        height="45"
+        className="d-inline-block align-top"
+        alt="Spots-logo"
+      />
+                <p>Spots is a chat app for people to connect together and share their stories, inspirations, ideas, hobbies and talents. The app is for people on work groups, language groups, gaming clans, school groups, and anything like that.</p>
+                <h5>© 2022 Spots, All rights reserved</h5>
+            </div>
+        
+            <div className="col-md-2 footer-social animated fadeInDown">
+            	<h4>Links</h4>
+            	<ul>
+                	<li><a href="#about">About Us</a></li>
+                	<li><a href="#">Contacts</a></li>
+                	<li><a href="#">Terms & Conditions</a></li>
+                	<li><a href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+        	<div className="col-md-2 footer-social animated fadeInDown">
+            	<h4>Follow Us</h4>
+            	<ul>
+                	<li><a href="#">Facebook</a></li>
+                	<li><a href="#">Twitter</a></li>
+                	<li><a href="#">Instagram</a></li>
+                	<li><a href="#">Email</a></li>
+                </ul>
+            </div>
+        	<div className="col-md-3 footer-ns animated fadeInRight">
+            	<h4>Newsletter</h4>
+                <h6 className="para">Find Your Spot In The World & Explore Others, Get Inpired !</h6>
+                <p>
+                    <div className="input-group">
+                      {/* <input type="text" className="form-control" placeholder="Search for..."/>
+                      <span className="input-group-btn">
+                        <button className="btn btn-default" type="button"><span className="glyphicon glyphicon-envelope"></span></button>
+                      </span> */}
+                    </div>
+                 </p>
+            </div>
+        </div>
+    </footer>
+</div>
+  </div>;
 }
 
 export default Footer;
